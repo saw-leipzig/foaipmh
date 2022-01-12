e66a849925281d4454bcfdae1bfa8dfba6a43dd5
Felix Helfer <helfer@informatik.uni-leipzig.de>
2022-01-12 13:50:15 +0100

    allow for recursive traversal of repo resources

--- foaipmh/management/commands/import.py
+++ foaipmh/management/commands/import.py
@@ -18,6 +18,7 @@ class Command(BaseCommand):
     key_identifier = "http://purl.org/dc/elements/1.1/identifier"
     key_created = "http://fedora.info/definitions/v4/repository#created"
     key_memberof = "http://purl.org/dc/elements/1.1/memberOf"
+    sets = {}
 
     def add_arguments(self, parser):
         """Add arguments."""
@@ -26,7 +27,7 @@ class Command(BaseCommand):
             "--sleep",
             type=int,
             default=5,
-            help="Sleep intervall in sec. To prevent to many requests in short time. "
+            help="Sleep interval in sec. To prevent too many requests in short time. "
             + "Default: 5",
         )
 
@@ -58,79 +59,11 @@ class Command(BaseCommand):
         nb_sets = Set.objects.count()
         nb_headers = Header.objects.count()
 
-        sets = {}
-        for i in r.json()[0]["http://www.w3.org/ns/ldp#contains"]:
-            if verbosity >= 2:
-                self.stdout.write(f"Fetch from {i['@id']}.")
-            r = requests.get(
-                i["@id"],
-                auth=settings.FEDORA_AUTH,
-                headers={"Accept": "application/ld+json"},
-            )
+        self.sets = {}
 
-            data = r.json()[0]
+        self.fetch_from_id(settings.FEDORA_REST_ENDPOINT, metadata_formats, verbosity, options["sleep"])
 
-            if self.key_set_spec in data and self.key_set_name in data:
-                if verbosity > 2:
-                    self.stdout.write(
-                        f"Create set {data[self.key_set_name][0]['@value']}."
-                    )
-                Set.objects.update_or_create(
-                    spec=data[self.key_set_spec][0]["@value"],
-                    name=data[self.key_set_name][0]["@value"],
-                )
-            else:
-                if self.key_identifier in data:
-                    identifier = data[self.key_identifier][0]["@value"]
-                else:
-                    identifier = f"oai:{i['@id']}"
-                if "self.key_created" in data:
-                    timestamp = data["self.key_created"][0]["@value"]
-                else:
-                    timestamp = None
-                if self.key_memberof in data:
-                    setspec = data[self.key_memberof][0]["@value"]
-                else:
-                    setspec = None
-
-                if verbosity > 2:
-                    self.stdout.write(f"Create header {identifier}.")
-                header, created = Header.objects.update_or_create(
-                    identifier=identifier,
-                    defaults={
-                        "timestamp": timestamp,
-                    },
-                )
-                if setspec:
-                    sets[header] = setspec
-
-                for k, v in settings.FEDORA_METADATA_SUFFIX.items():
-                    if verbosity > 2:
-                        self.stdout.write(f"Fetch from {i['@id']}{v}.")
-                    r = requests.get(
-                        f'{i["@id"]}{v}',
-                        auth=settings.FEDORA_AUTH,
-                    )
-                    if r.status_code == requests.codes.ok:
-                        if verbosity > 2:
-                            self.stdout.write(f"Add metadata format of type {k}.")
-                        if k == "oai_dc":
-                            dcrecord, created = DCRecord.from_xml(r.text, header)
-                            if dcrecord:
-                                header.metadata_formats.add(
-                                    MetadataFormat.objects.get(prefix="oai_dc")
-                                )
-                        else:
-                            XMLRecord.objects.update_or_create(
-                                header=header,
-                                metadata_prefix=metadata_formats[k],
-                                defaults={"xml_metadata": r.text},
-                            )
-                            header.metadata_formats.add(metadata_formats[k])
-
-            sleep(options["sleep"])
-
-        for k, v in sets:
+        for k, v in self.sets.items():
             try:
                 k.sets.add(Set.objects.get(spec=v))
             except Set.DoesNotExist:
@@ -144,3 +77,80 @@ class Command(BaseCommand):
         if verbosity >= 1:
             self.stdout.write(f"Added {nb_sets} sets.")
             self.stdout.write(f"Added {nb_headers} headers.")
+
+    def fetch_from_id(self, fedora_id, metadata_formats, verbosity, sleep_time):
+        if verbosity >= 2:
+            self.stdout.write(f"Fetch from {fedora_id}.")
+        r = requests.get(
+            fedora_id,
+            auth=settings.FEDORA_AUTH,
+            headers={"Accept": "application/ld+json"},
+        )
+
+        if not r:
+            return
+
+        data = r.json()[0]
+
+        if self.key_set_spec in data and self.key_set_name in data:
+            if verbosity > 2:
+                self.stdout.write(
+                    f"Create set {data[self.key_set_name][0]['@value']}."
+                )
+            Set.objects.update_or_create(
+                spec=data[self.key_set_spec][0]["@value"],
+                name=data[self.key_set_name][0]["@value"],
+            )
+        else:
+            if self.key_identifier in data:
+                identifier = data[self.key_identifier][0]["@value"]
+            else:
+                identifier = f"oai:{fedora_id}"
+            if "self.key_created" in data:
+                timestamp = data["self.key_created"][0]["@value"]
+            else:
+                timestamp = None
+            if self.key_memberof in data:
+                setspec = data[self.key_memberof][0]["@value"]
+            else:
+                setspec = None
+
+            if verbosity > 2:
+                self.stdout.write(f"Create header {identifier}.")
+            header, created = Header.objects.update_or_create(
+                identifier=identifier,
+                defaults={
+                    "timestamp": timestamp,
+                },
+            )
+            if setspec:
+                self.sets[header] = setspec
+
+            for k, v in settings.FEDORA_METADATA_SUFFIX.items():
+                if verbosity > 2:
+                    self.stdout.write(f"Fetch from {fedora_id}{v}.")
+                r_metadata = requests.get(
+                    f'{fedora_id}{v}',
+                    auth=settings.FEDORA_AUTH,
+                )
+                if r_metadata.status_code == requests.codes.ok:
+                    if verbosity > 2:
+                        self.stdout.write(f"Add metadata format of type {k}.")
+                    if k == "oai_dc":
+                        dcrecord, created = DCRecord.from_xml(r_metadata.text, header)
+                        if dcrecord:
+                            header.metadata_formats.add(
+                                MetadataFormat.objects.get(prefix="oai_dc")
+                            )
+                    else:
+                        XMLRecord.objects.update_or_create(
+                            header=header,
+                            metadata_prefix=metadata_formats[k],
+                            defaults={"xml_metadata": r_metadata.text},
+                        )
+                        header.metadata_formats.add(metadata_formats[k])
+
+        sleep(sleep_time)
+        if "http://www.w3.org/ns/ldp#contains" in r.json()[0]:
+            for i in r.json()[0]["http://www.w3.org/ns/ldp#contains"]:
+                self.fetch_from_id(i['@id'], metadata_formats, verbosity, sleep_time)
