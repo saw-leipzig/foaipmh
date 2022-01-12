7ea4a8991cc4ce98226d3a93acc5f101427ced4e
Felix Helfer <helfer@informatik.uni-leipzig.de>
2022-01-12 13:36:24 +0100

    fix cmdi namespace and schema

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -2,6 +2,6 @@
 
 python manage.py migrate --noinput
 python manage.py collectstatic --no-input
-python manage.py shell --command "from oai_pmh.models import MetadataFormat; print(MetadataFormat.objects.get_or_create(prefix='oai_cmdi', schema='http://www.clarin.eu/cmd/1', namespace='https://catalog.clarin.eu/ds/ComponentRegistry/rest/registry/1.x/profiles/clarin.eu:cr1:p_1527668176047/xsd'))"
+python manage.py shell --command "from oai_pmh.models import MetadataFormat; print(MetadataFormat.objects.get_or_create(prefix='cmdi', schema='https://infra.clarin.eu/CMDI/1.x/xsd/cmd-envelop.xsd', namespace='http://www.clarin.eu/cmd/1''))"
 
 exec "$@"
